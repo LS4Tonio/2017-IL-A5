6b444cab2ba5d26f2e98cd07bf043e52ded9ada4
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-06-16 10:56:54 +0200

    SA works.

--- Algo/Algo.Optim/SolutionSpace.cs
+++ Algo/Algo.Optim/SolutionSpace.cs
@@ -28,25 +28,45 @@ namespace Algo.Optim
 
         public int[] Cardinalities { get; private set; }
 
+        public Random Random => _random;
+
         public SolutionInstance GetRandomInstance()
         {
             int[] coord = new int[Dimension];
             for(int i = 0; i < Dimension; ++i )
             {
-                coord[i] = _random.Next(Cardinalities[i]);
+                coord[i] = Random.Next(Cardinalities[i]);
             }
             return CreateSolutionInstance(coord);
         }
 
         public void TryRandom(int nbTry, bool useMonteCarlo = false)
         {
-            double forceCompueCost;
+            double forceComputeCost;
             while (--nbTry >= 0)
             {
                 var r = GetRandomInstance();
                 if (useMonteCarlo) r = r.MonteCarloPath().Last();
-                else forceCompueCost = r.Cost;
+                else forceComputeCost = r.Cost;
             }
+        }
+        public Tuple<int,int,int> TrySimulatedAnnealing(int nbTry, int countPerTempDecrease)
+        {
+            int aWin = 0;
+            int amWin = 0;
+            int maWin = 0;
+            while (--nbTry >= 0)
+            {
+                var r = GetRandomInstance();
+                var a = r.SimulatedAnnealing( countPerTempDecrease );
+                var m = r.MonteCarloPath().Last();
+                var ma = m.SimulatedAnnealing(countPerTempDecrease);
+                var am = a.MonteCarloPath().Last();
+                if (a.Cost < m.Cost) ++aWin;
+                if (am.Cost < a.Cost) ++amWin;
+                if (ma.Cost < a.Cost) ++maWin;
+            }
+            return Tuple.Create( aWin, amWin, maWin);
         }
 
         internal protected abstract SolutionInstance CreateSolutionInstance(int[] coord);
